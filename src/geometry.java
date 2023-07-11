b12743ca94a4b854c0e1de8c3a0d35993e48a4c0
rajeeva <rajeevabp92@gmail.com>
2023-07-11 21:31:09 +0530

    Used the scanner

---
+++ src/geometry.java
@@ -0,0 +1,11 @@
+public class geometry {
+    public static float area(float radius){
+        return 3.141f * radius * radius;
+    }
+}
+
+class rectangle{
+    public static float area(float length, float breadth){
+        return length * breadth;
+    }
+}
